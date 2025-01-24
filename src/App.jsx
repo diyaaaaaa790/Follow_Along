7a4469ebe57a5cac75663aa3b5b4529f4fb801e9
DIYA <diya94816@gmail.com>
2025-01-24 11:36:39 +0530

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,SignupPage} from "./Routes";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+    
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
