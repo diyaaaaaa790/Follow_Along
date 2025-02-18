6483dfdd1ef1aba4174a775ee46900be8f6d8027
DIYA <diya94816@gmail.com>
2025-02-18 11:03:09 +0530

    milestone 17

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -3,8 +3,7 @@ import LoginPage from "./pages/Login.jsx";
 import SignupPage from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
 import CreateProduct from "./pages/createProduct.jsx";
-import myProducts from "./pages/myProducts.jsx";
+import MyProducts from "./pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
 import ProductDetails from "./pages/productDetails.jsx";
-
-export { SignupPage, LoginPage, Home, CreateProduct, myProducts, Cart, ProductDetails};
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails };
