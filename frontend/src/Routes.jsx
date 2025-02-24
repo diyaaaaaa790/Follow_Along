1146763b0a0a31faa714d112a21e8ec40e4981a1
DIYA <diya94816@gmail.com>
2025-02-24 15:19:04 +0530

    committed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,4 @@
 
-
 import LoginPage from "./pages/Login.jsx";
 import SignupPage from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
@@ -7,6 +6,9 @@ import CreateProduct from "./pages/createProduct.jsx";
 import MyProducts from "./pages/myProducts.jsx";
 import Cart from "./pages/cart.jsx";
 import ProductDetails from "./pages/productDetails.jsx";
-import Profile from "./pages/profile.jsx"
-export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails,Profile };
+import Profile from "./pages/Profile.jsx"
+import CreateAddress from "./pages/createAddress.jsx";
 
+
+
+export { SignupPage, LoginPage, Home, CreateProduct, MyProducts, Cart, ProductDetails,Profile,CreateAddress};
